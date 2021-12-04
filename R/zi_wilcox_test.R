e0ac634ba823048097b83774a1eb33104c8d44a0
slamballais <s.lamballaistessensohn@erasmusmc.nl>
2021-12-04 12:17:25 +0100

    move calculate_ziw_statistic out of function body

--- R/zi_wilcox_test.R
+++ R/zi_wilcox_test.R
@@ -16,36 +16,6 @@
 #' @references Wanjie Wang, Eric Z. Chen and Hongzhe Li (2015). Rank-based tests for compositional distributions with a clump of zeros. Submitted.
 
 ziw = function(x, y, perm = FALSE, perm.n = 10000) {
-  calculate_ziw_statistic = function(x,y){
-    ## total observations in each vector
-    N <- c(length(x), length(y))
-    ## number of non-zero observations in each vector
-    n <- c(sum(x != 0), sum(y != 0))
-    ## non-zero proportion
-    prop <- n / N 
-    pmax <- max(prop) 
-    pmean <- mean(prop)
-    ## keep only round(pmax * N) observations in each group
-    Ntrun <- round(pmax * N)
-    ## truncate zeros in X and Y
-    Xtrun <- c(x[x != 0], rep(0, Ntrun[1] - n[1]))
-    Ytrun <- c(y[y != 0], rep(0, Ntrun[2] - n[2]))
-    rankdata <- sum(Ntrun) + 1 - rank(c(Xtrun, Ytrun))
-    ## mean of the modified wilcoxon rank sum statistic
-    r <- sum(rankdata[1:Ntrun[1]])
-    s <- r - Ntrun[1] * (sum(Ntrun) + 1) / 2
-    ## variance of the modified wilcoxon rank sum statistic
-    v1   <- pmean * (1 - pmean)
-    var1 <- N[1] * N[2] * pmean * v1 * (sum(N) * pmean + 3 * (1 - pmean) / 2) + 
-      sum(N^2) * v1^2 * 5 / 4 + 2 * sqrt(2 / pi) * pmean * (sum(N) * v1) ^ (1.5) *
-      sqrt(N[1] * N[2])
-    var1 <- var1 / 4
-    var2 <- N[1]*N[2]*pmean^2*(sum(N)*pmean+1)/12
-    vars <- var1 + var2
-    ## modified wilcoxon rank sum statistic
-    w <- s / sqrt(vars)
-    return(w)
-  }
   
   ## calclulate the ziw statistic
   w <- calculate_ziw_statistic(x,y)
@@ -70,4 +40,35 @@ ziw = function(x, y, perm = FALSE, perm.n = 10000) {
   }
   
   return(list(p.value = p, statistics = w))
+}
+
+calculate_ziw_statistic = function(x,y){
+  ## total observations in each vector
+  N <- c(length(x), length(y))
+  ## number of non-zero observations in each vector
+  n <- c(sum(x != 0), sum(y != 0))
+  ## non-zero proportion
+  prop <- n / N 
+  pmax <- max(prop) 
+  pmean <- mean(prop)
+  ## keep only round(pmax * N) observations in each group
+  Ntrun <- round(pmax * N)
+  ## truncate zeros in X and Y
+  Xtrun <- c(x[x != 0], rep(0, Ntrun[1] - n[1]))
+  Ytrun <- c(y[y != 0], rep(0, Ntrun[2] - n[2]))
+  rankdata <- sum(Ntrun) + 1 - rank(c(Xtrun, Ytrun))
+  ## mean of the modified wilcoxon rank sum statistic
+  r <- sum(rankdata[1:Ntrun[1]])
+  s <- r - Ntrun[1] * (sum(Ntrun) + 1) / 2
+  ## variance of the modified wilcoxon rank sum statistic
+  v1   <- pmean * (1 - pmean)
+  var1 <- N[1] * N[2] * pmean * v1 * (sum(N) * pmean + 3 * (1 - pmean) / 2) + 
+    sum(N^2) * v1^2 * 5 / 4 + 2 * sqrt(2 / pi) * pmean * (sum(N) * v1) ^ (1.5) *
+    sqrt(N[1] * N[2])
+  var1 <- var1 / 4
+  var2 <- N[1]*N[2]*pmean^2*(sum(N)*pmean+1)/12
+  vars <- var1 + var2
+  ## modified wilcoxon rank sum statistic
+  w <- s / sqrt(vars)
+  return(w)
 }
